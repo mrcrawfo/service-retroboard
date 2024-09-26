9bb787c2378b9cf9c66dd5089ee417b75ba36911
Max Crawford <mrcrawfo@gmail.com>
2024-09-26 02:24:56 -0400

    fix(routes): Enforce jwt expiration logout on page navigation

--- src/app/routes/_authenticated.tsx
+++ src/app/routes/_authenticated.tsx
@@ -1,8 +1,38 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
+import { GET_USER_DATA } from '../graph/auth/queries.js';
+
 export const Route = createFileRoute('/_authenticated')({
-    beforeLoad: ({ context }) => {
+    beforeLoad: async ({ context }) => {
+        // no cached user info, redirect to login
         if (!context.user || !context.token) {
+            throw redirect({ to: '/login' });
+        }
+
+        // confirm cached user info is valid
+        const response = await context.client.query({
+            query: GET_USER_DATA,
+            fetchPolicy: 'network-only',
+            context: {
+                headers: {
+                    authorization: `Bearer ${context.token}`,
+                },
+            },
+        });
+
+        if (response?.data?.me) {
+            // If no userData (token is expired) then override locally stored values and redirect to login page
+            if (!response.data.me.success && response.data.me.message === 'Token expired') {
+                context.setUser(null);
+                context.setToken(null);
+
+                throw redirect({ to: '/login' });
+            }
+        }
+
+        const userData = response?.data?.me?.user || null;
+
+        if (!userData) {
             throw redirect({ to: '/login' });
         }
     },
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { useApolloClient } from '@apollo/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 import { ApolloWrapper } from './app/components/atoms/ApolloWrapper.js';
 import ApolloAuthProvider from './app/hocs/ApolloAuthProvider.js';
-import { useAuthStoreToken, useAuthStoreUser } from './app/store/AuthStore.js';
+import { useAuthStoreActions, useAuthStoreToken, useAuthStoreUser } from './app/store/AuthStore.js';
 
 // Import the generated route tree
 import { routeTree } from './app/routeTree.gen.js';
 
 // Create a new router instance
-const router = createRouter({ routeTree, context: { user: undefined!, token: undefined! } });
+const router = createRouter({
+    routeTree,
+    context: { user: undefined!, token: undefined!, setUser: undefined!, setToken: undefined!, client: undefined! },
+});
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -20,18 +24,24 @@ declare module '@tanstack/react-router' {
 }
 
 function App() {
-    const user = useAuthStoreUser();
-    const token = useAuthStoreToken();
-
     return (
         <StrictMode>
             <ApolloWrapper>
                 <ApolloAuthProvider>
-                    <RouterProvider router={router} context={{ user, token }} />
+                    <AppContextWrapper />
                 </ApolloAuthProvider>
             </ApolloWrapper>
         </StrictMode>
     );
+}
+
+function AppContextWrapper() {
+    const user = useAuthStoreUser();
+    const token = useAuthStoreToken();
+    const client = useApolloClient();
+    const { setUser, setToken } = useAuthStoreActions();
+
+    return <RouterProvider router={router} context={{ user, token, setUser, setToken, client }} />;
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
